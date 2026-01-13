ea319c8a0d8d1dc099a8ca862e18862908f40c3c
Cássio Lima <kcyo2012@gmail.com>
2026-01-13 16:12:23 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,30 @@
+// const CACHE_NAME = 'radio-v1';
+// const ASSETS = [
+//   './',
+//   './index.html',
+//   './manifest.json'
+// ];
+
+// // Instala o Service Worker e armazena os arquivos básicos em cache
+// self.addEventListener('install', (e) => {
+//   e.waitUntil(
+//     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+//   );
+// });
+
+// // Responde com o cache quando estiver offline
+// self.addEventListener('fetch', (e) => {
+//   e.respondWith(
+//     caches.match(e.request).then((response) => response || fetch(e.request))
+//   );
+// });
 const CACHE_NAME = 'radio-v1';
 const ASSETS = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './icon_192x192.png',
+  './icon_512x512.png'
 ];
 
 // Instala o Service Worker e armazena os arquivos básicos em cache
